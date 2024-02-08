2e9314724427a303382e7c92a5e168501b3381c3
arkoaikins <arkoaikins@gmail.com>
2024-02-08 16:59:54 -0500

    [pycodestyle]: fixed indentation error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,5 +18,5 @@ def filter_datum(fields: List[str],
     """
     for field in fields:
         message = re.sub(field+'=.*?'+separator,
-                             field+'='+redaction+separator, message)
+                         field+'='+redaction+separator, message)
     return message
