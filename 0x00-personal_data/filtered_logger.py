41b49eebbeed47151694c24070b7b71bd2001c43
arkoaikins <arkoaikins@gmail.com>
2024-02-08 16:54:14 -0500

    write function that returns log message obfuscated

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Function that returns the log message obfuscated
+Args:
+fields: a list of strings representing all fields of obfuscate
+redaction: a string representing by what the field will be obfuscated
+message: a string representing by which character is seperating all
+        fields in the log line(message)
+"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str],
+                 redaction: str, message: str, separator: str) -> str:
+    """
+    Returns  the log message obfuscated
+    """
+    for field in fields:
+        log_message = re.sub(field+'=.*?'+separator,
+                             field+'='+redaction+separator, message)
+    return log_message
