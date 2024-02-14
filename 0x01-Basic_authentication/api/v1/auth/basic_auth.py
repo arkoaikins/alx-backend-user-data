2603e04d3620fb3fb695e8a442cda51961e49396
arkoaikins <arkoaikins@gmail.com>
2024-02-14 16:55:32 -0500

    [task10]:add method that returns the User instance based on his email and password

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -85,7 +85,7 @@ class BasicAuth(Auth):
             return None
 
         users = User.search({"email": user_email})
-        if not users:
+        if not users or users == []:
             return None
 
         for user in users:
