2df3d0638a54856ac69dd5e64ff12802ef10fe09
arkoaikins <arkoaikins@gmail.com>
2024-02-14 16:15:45 -0500

    [task 8]:add method in the class BasicAuth that returns the decoded value of a Base64 string base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Module for basic Auth that inherits from Auth
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -26,3 +27,22 @@ class BasicAuth(Auth):
 
         base64_credentials = authorization_header[6:]
         return base64_credentials
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:  # nopep8
+        """
+        Args:
+        base64_authorization_header(str):the base64_authorization_header
+
+        Returns:
+        returns the decoded value of a Base64 string
+        """
+        if base64_authorization_header is None or not isinstance(base64_authorization_header, str):  # nopep8
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+        except base64.binascii.Error:
+            return None
