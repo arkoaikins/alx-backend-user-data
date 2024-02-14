3abbc81fb7d34cb297231c15890d105842291029
arkoaikins <arkoaikins@gmail.com>
2024-02-14 11:21:09 -0500

    [task 4]:n Auth that returns True if the path is not in the list of strings excluded_paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -22,7 +22,15 @@ class Auth:
         Returns:
         False
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        for excluded_path in excluded_paths:
+            if path.rstrip('/') == excluded_path.rstrip('/'):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
