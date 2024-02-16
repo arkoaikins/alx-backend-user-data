fcc336a322a797cd300b7f86ad498af45633e087
arkoaikins <arkoaikins@gmail.com>
2024-02-16 14:36:18 -0500

    task2]:Create an instance method def create_session(self, user_id: str = None) -> str: that creates a Session ID for a user_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,8 +5,21 @@ SessionAuth that interits from Auth
 
 """
 from api.v1.auth.auth import Auth
+from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
     """new authentication mechanism"""
-    pass
+    user_id_by_session_id = {}
+    
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a Session ID for a user_id
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
