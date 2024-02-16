dd3bc00c842fefc498b70068da76861093074f6b
arkoaikins <arkoaikins@gmail.com>
2024-02-16 13:31:22 -0500

    Create a class SessionAuth that inherits from Auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+New authentication mechanism
+SessionAuth that interits from Auth
+
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """new authentication mechanism"""
+    pass
