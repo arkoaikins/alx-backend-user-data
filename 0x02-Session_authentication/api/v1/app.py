44d06776cdff1bbe50eb64b6b257bda07af62d95
arkoaikins <arkoaikins@gmail.com>
2024-02-16 13:36:34 -0500

    update

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -17,11 +17,11 @@ auth = None
 if getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif getenv('AUTH_TYPE') == 'basic_auth':
+if getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
-elif getenv('AUTH_TYPE') == 'sesssion_auth':
+if getenv('AUTH_TYPE') == 'sesssion_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 
