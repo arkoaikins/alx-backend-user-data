a092631c3dbee85f2d7f1b1a3dadd05e52c5efcc
arkoaikins <arkoaikins@gmail.com>
2024-02-21 16:46:51 -0500

    create a basic flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app
+"""
+from flask import Flask, jsonify, request, abort, redirect, url_for
+from auth import Auth
+
+app = Flask(__name__)
+Auth = Auth()
+
+
+@app.route("/", method=["Get"])
+def index():
+    """home"""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
