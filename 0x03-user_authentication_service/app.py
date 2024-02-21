e1713707ee7a2f87054f257c59642049092ae994
arkoaikins <arkoaikins@gmail.com>
2024-02-21 17:27:23 -0500

    create method that adds user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -15,5 +15,17 @@ def index():
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route("/users", methods=["POST"])
+def register_user():
+    """Register user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        Auth.register_user(email, password)
+        return jsonify({"email": "<registered email>", "message": "user created"})  # nopep8
+    except ValueError:
+        return jsonify({"message": "email already registered"})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
