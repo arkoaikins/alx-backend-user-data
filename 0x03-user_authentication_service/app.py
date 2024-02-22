7a7535de24a23833172801e9c43e773b8e0682ec
arkoaikins <arkoaikins@gmail.com>
2024-02-22 12:41:45 -0500

    [task11]:implement a login function to respond to the POST /sessions route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -35,11 +35,11 @@ def login() -> dict:
     valid = Auth.valid_login(email, password)
     if not valid:
         abort(401)
-
-    session_id = Auth.create_session(email)
-    response = jsonify({"email": email, "message": "logged in"})
-    response.set_cookie("session_id", session_id)
-    return response
+    else:
+        session_id = Auth.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
 
 
 if __name__ == "__main__":
